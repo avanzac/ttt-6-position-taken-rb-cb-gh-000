6e5d3eece52f7dbe17fbf22951f658a7dd171290
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-21 18:52:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
-def position_taken?(index)
+def position_taken?(board, index)
   !(position_taken?(board, index).nil? || position_taken?(board, index) == " ")
 end
